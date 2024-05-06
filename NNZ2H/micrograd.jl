838b538ff8053ae079b1f66131f286bf35c7f8cd
Jeremy E Kozdon <jeremy@kozdon.net>
2024-05-05 19:33:43 -0700

    adding neural network stuff
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- NNZ2H/micrograd.jl
+++ NNZ2H/micrograd.jl
@@ -47,6 +47,9 @@ mutable struct Value{T <: AbstractFloat}
 			isnothing(backward) ? () -> zero(T) : backward
 		)
 	end
+	function Value(data::T) where T<:AbstractFloat
+		return Value(;data=data)
+	end
 end
 
 # ╔═╡ a76c175e-19a9-444f-8590-13e4b57de8c5
@@ -74,6 +77,12 @@ function Base.:*(x::Value{T}, y::Value{T}) where T
 	end
     return out
 end
+
+# ╔═╡ 0abb4567-8665-4c4d-b4ea-093d87656e2c
+Base.:*(x::Value{T}, y::T) where T = x * Value(y)
+
+# ╔═╡ fb1aa33d-7370-408f-bd85-22cd1da189b7
+Base.:*(x::T, y::Value{T}) where T = Value(x) * y
 
 # ╔═╡ 1f13d05d-f2bc-4134-a344-5352f7e4e27c
 function Base.:tanh(x::Value{T}) where T 
@@ -171,41 +180,80 @@ function build_graph(v::Value, graph::String)
 	return graph
 end
 
-# ╔═╡ 00a4740f-f845-4f1a-995b-3a297cfeb1c9
-a = Value(data=-1/4., label="a")
+# ╔═╡ 4d896a33-68a5-4e9a-bcc1-8a768ec572cf
+struct Neuron{T <: AbstractFloat, N}
+	w::NTuple{N, Value{T}}
+	b::Value{T}
+	activation::Function
+	function Neuron{T, N}(;activation=tanh) where T <: AbstractFloat where N
+		w = ntuple(_->Value(rand(T)), N)
+		b = Value(rand(T))
+		new{T,N}(w,b,activation)
+	end
+end
 
-# ╔═╡ 30034c69-2f7a-48c0-92d8-3d161dafa2f8
-b = tanh(a);
+# ╔═╡ 765a1144-5fe5-400b-b02d-080cc7fcce88
+function (n::Neuron{T, N})(x) where {T, N}
+	@assert(N == length(x))
+	n.activation(mapreduce(wx -> wx[1] * wx[2], +, zip(n.w, x)) + n.b)
+end
 
-# ╔═╡ b904f613-6f09-4479-81aa-c912e04a4c13
-c = b + a;
+# ╔═╡ 9f0e6e55-285e-4c86-a1ec-e66a37b7331e
+struct Layer{T<:AbstractFloat, Nin, Nout}
+	data::NTuple{Nout, Neuron{T, Nin}}
+	function Layer{T, Nin, Nout}(;activation=tanh) where {T, Nin, Nout}
+		new(ntuple(_->Neuron{T, Nin}(;activation=activation), Nout))
+	end
+end
 
-# ╔═╡ 2d237e02-5af0-4d08-9e5b-8ae7b2420836
-d = tanh(b);
+# ╔═╡ a65f1980-25f1-42e5-ba22-4437604036ed
+function (l::Layer{T, Nin, Nout})(x) where {T, Nin, Nout}
+	map(n->n(x), l.data)
+end
 
-# ╔═╡ e9484c38-1785-4a96-b7fb-1886d2fa737c
-e = c * d;
+# ╔═╡ b9a6f0d8-d2e8-402c-ba84-bb66f04f3d0c
+struct MLP{T<:AbstractFloat, Nin}
+	data::Vector{Layer{T}}
+	function MLP{T}(d::I...) where {T, I}
+		new{T, d[1]}(map(N->Layer{T, N[1], N[2]}(), zip(d, (d[2:end]..., 1))))
+	end
+end
 
-# ╔═╡ 91aa1620-572e-488a-8451-ba3cb4c8b63e
-backward_propagate(e)
+# ╔═╡ 97379022-055c-4770-afc7-22d56a4d0a25
+function (mlp::MLP{T, Nin})(x) where {T, Nin}
+	for i in 1:length(mlp.data)
+	  x = mlp.data[i](x)
+	end
+	x[1]
+end
 
-# ╔═╡ 3c9e1ff6-9ce6-4f05-96fb-70ee40052409
-build_graph(e)
+# ╔═╡ 18e7b40e-e965-43be-9284-d615cd4d5e57
+m = MLP{Float64}(3,4,5)
+
+# ╔═╡ 63fee104-6bb7-4d1a-ad74-9e4b8ce0fade
+x = rand(3)
+
+# ╔═╡ e34800e4-7613-44fb-bf95-8d458ce52f49
+build_graph(m(x))
 
 # ╔═╡ Cell order:
 # ╠═2fc9f05e-b170-4575-8dd4-ac77f64494fc
 # ╠═e5e1afe4-43ce-435e-b734-e7321ca8bfd8
 # ╠═a76c175e-19a9-444f-8590-13e4b57de8c5
 # ╠═e6a22cc9-8543-44fe-97de-d76f3354ceb6
+# ╠═0abb4567-8665-4c4d-b4ea-093d87656e2c
+# ╠═fb1aa33d-7370-408f-bd85-22cd1da189b7
 # ╠═1f13d05d-f2bc-4134-a344-5352f7e4e27c
 # ╠═0c957a70-a334-4ded-8131-db1ef41638ca
 # ╠═6bbaee3d-b446-4647-b057-5c453dd1e5cf
 # ╠═9c66b761-fffe-43f4-b8f2-ab0e0b78f291
 # ╠═cb3978fe-7221-4e63-9814-b03150e8c398
-# ╠═00a4740f-f845-4f1a-995b-3a297cfeb1c9
-# ╠═30034c69-2f7a-48c0-92d8-3d161dafa2f8
-# ╠═b904f613-6f09-4479-81aa-c912e04a4c13
-# ╠═2d237e02-5af0-4d08-9e5b-8ae7b2420836
-# ╠═e9484c38-1785-4a96-b7fb-1886d2fa737c
-# ╠═91aa1620-572e-488a-8451-ba3cb4c8b63e
-# ╠═3c9e1ff6-9ce6-4f05-96fb-70ee40052409
+# ╠═4d896a33-68a5-4e9a-bcc1-8a768ec572cf
+# ╠═765a1144-5fe5-400b-b02d-080cc7fcce88
+# ╠═9f0e6e55-285e-4c86-a1ec-e66a37b7331e
+# ╠═a65f1980-25f1-42e5-ba22-4437604036ed
+# ╠═b9a6f0d8-d2e8-402c-ba84-bb66f04f3d0c
+# ╠═97379022-055c-4770-afc7-22d56a4d0a25
+# ╠═18e7b40e-e965-43be-9284-d615cd4d5e57
+# ╠═63fee104-6bb7-4d1a-ad74-9e4b8ce0fade
+# ╠═e34800e4-7613-44fb-bf95-8d458ce52f49
